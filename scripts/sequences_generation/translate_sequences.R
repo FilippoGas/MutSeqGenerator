58487818a979bbc498f6ec1ffdcb5234743e52cc
FilippoGas <filippogastaldello@gmail.com>
2025-10-21 12:03:01 +0200

    Parallelized sequence translation

--- scripts/sequences_generation/translate_sequences.R
+++ scripts/sequences_generation/translate_sequences.R
@@ -4,18 +4,22 @@
 
 library(tidyverse)
 library(Biostrings)
+library(parallel)
+
 # Load haplotypes and sequences
 haplotype_ID <- read_csv(snakemake@input[["haplotypes"]])
-
 # Extract chromosome name from input files
 chr <- str_split_i(snakemake@input[["mutated_cds"]], pattern = "chr", 2) %>% str_split_i(pattern = ".csv", 1)
+# Read cores from snakemake
+cores <- snakemake@threads
 
 # Cut the sequences to make them start from the first starting codon
-haplotype_ID <- haplotype_ID %>% mutate(start_codon_pos = str_locate(nn_sequence, "ATG")[,"start"],
-                                        start_codon_pos = ifelse(is.na(start_codon_pos), 1, start_codon_pos),
-                                        ATG_nn_sequences = str_sub(nn_sequence, start_codon_pos, nchar(nn_sequence)))
-res <- lapply(haplotype_ID$ATG_nn_sequences, function(x) {as.character(translate(DNAString(x)))})
-haplotype_ID$aa_sequence <- as.character(res)
+res <- mclapply(haplotype_ID$nn_sequence,
+                function(x) {data.frame("aa_sequence" = as.character(translate(DNAString(x))))},
+                mc.cores = cores,
+                mc.preschedule = TRUE,
+                mc.cleanup = TRUE)
+haplotype_ID$aa_sequence <- bind_rows(res)$aa_sequence
 # Cut sequences after the first stop codon
 haplotype_ID <- haplotype_ID %>% mutate(aa_sequence = str_split_i(aa_sequence, "\\*",1))
 # Save result
